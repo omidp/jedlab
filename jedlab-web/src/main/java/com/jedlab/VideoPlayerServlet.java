d5216e35ad2f2834714afcff06a7ad15e8203649
omidp <omidpourhadi@gmail.com>
2016-08-21 22:30:51 +0430

    video servlet async support improvement

--- jedlab-web/src/main/java/com/jedlab/VideoPlayerServlet.java
+++ jedlab-web/src/main/java/com/jedlab/VideoPlayerServlet.java
@@ -3,6 +3,7 @@ package com.jedlab;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,11 +17,10 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.jboss.seam.Component;
-import org.jboss.seam.annotations.FlushModeType;
 import org.jboss.seam.contexts.Contexts;
-import org.jboss.seam.persistence.PersistenceContexts;
+import org.jboss.seam.contexts.Lifecycle;
+import org.jboss.seam.contexts.ServletLifecycle;
 import org.jboss.seam.security.Identity;
-import org.jboss.seam.servlet.ContextualHttpServletRequest;
 
 import com.jedlab.action.Constants;
 import com.jedlab.framework.MultipartFileSender;
@@ -36,79 +36,107 @@ public class VideoPlayerServlet extends HttpServlet
 
     private final static Logger LOGGER = Logger.getLogger(VideoPlayerServlet.class.getName());
 
+    private static ThreadLocal<AtomicInteger> count = new ThreadLocal<AtomicInteger>();
+
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
     {
-        new ContextualHttpServletRequest(req) {
-
-            @Override
-            public void process() throws Exception
-            {
-                doWork(req, resp);
-            }
-        }.run();
-    }
-
-    private static void doWork(HttpServletRequest req, HttpServletResponse resp) throws ServletException
-    {
+        if (getCounterValue() == 0)
+        {
+            // Lifecycle.setupApplication(new ServletApplicationMap(context));
+            ServletLifecycle.beginReinitialization(req, getServletContext());
+        }
 
         try
         {
-            Identity identity = Identity.instance();
-            if (identity.isLoggedIn() == false)
-                throw new RequestException("not loggedin");
-            Long uid = (Long) Contexts.getSessionContext().get(Constants.CURRENT_USER_ID);
-            if (uid == null)
-                throw new RequestException("not loggedin");
-            // only allowed partial content
-            String range = req.getHeader("Range");
-            if (StringUtil.isEmpty(range))
-                throw new RequestException("matcher not found");
-            Matcher matcher = pattern.matcher(range);
-            if (matcher.find() == false)
-                throw new RequestException("not in range");
-            // Long r = Long.parseLong(matcher.group(1));
+            incrementCounterValue();
 
-            Session sess = (Session) Component.getInstance("hibernateSession");
-            PersistenceContexts.instance().changeFlushMode(FlushModeType.MANUAL);
-            String token = getToken(req.getRequestURL().toString());
-            if (StringUtil.isEmpty(token))
-                throw new RequestException("invalid token");
-            Criteria criteria = sess.createCriteria(VideoToken.class, "vt");
-            criteria.createCriteria("vt.chapter", "chap", Criteria.LEFT_JOIN);
-            criteria.add(Restrictions.eq("vt.memberId", uid));
-            criteria.add(Restrictions.eq("vt.token", token));
-            criteria.setMaxResults(1);
-            VideoToken vt = (VideoToken) criteria.uniqueResult();
-            if (vt == null)
+            doWork(req, resp);
+
+            decrementCounterValue();
+
+            // End request only if it is not nested ContextualHttpServletRequest
+            if (getCounterValue() == 0)
             {
-                throw new RequestException("user not registered in course, can't find video token");
+                ServletLifecycle.endReinitialization();
+                // Lifecycle.cleanupApplication();
             }
-            Chapter chapter = vt.getChapter();
-            long expire = chapter.getDuration().getTime() + new Date().getTime();
-            //
-            String filePath = chapter.getUrl();
-            String res = req.getParameter("resolution");
-            filePath = getFileName(res, filePath);
-            File file = new File(filePath);
-            if (file.exists() == false)
-                throw new RequestException("file  not found");
-            MultipartFileSender.fromFile(file).with(req).with(resp).with(expire).serveResource();
-            // if (r >= file.length())
-            // {
-            // sess.createQuery("delete from VideoToken vt where vt.id = :vtId").setParameter("vtId",
-            // vt.getId()).executeUpdate();
-            // sess.flush();
-            // }
-
+        }
+        catch (IOException ioe)
+        {
+            removeCounter();
+            Lifecycle.endRequest();
+            // throw ioe;
+        }
+        catch (ServletException se)
+        {
+            removeCounter();
+            Lifecycle.endRequest();
+            // throw se;
         }
         catch (Exception e)
         {
-            LOGGER.info("exception occured VideServletPlayer : " + e.getMessage());
+            removeCounter();
+            Lifecycle.endRequest();
+            // throw new ServletException(e);
         }
-
+        finally
+        {
+            // request ended
+        }
     }
 
+    private void doWork(HttpServletRequest req, HttpServletResponse resp) throws Exception
+    {
+
+        Identity identity = Identity.instance();
+        if (identity.isLoggedIn() == false)
+            throw new RequestException("not loggedin");
+        Long uid = (Long) Contexts.getSessionContext().get(Constants.CURRENT_USER_ID);
+        if (uid == null)
+            sendError(resp);
+        // only allowed partial content
+        String range = req.getHeader("Range");
+        if (StringUtil.isEmpty(range))
+            sendError(resp);
+        Matcher matcher = pattern.matcher(range);
+        if (matcher.find() == false)
+            sendError(resp);
+        // Long r = Long.parseLong(matcher.group(1));
+
+        Session sess = (Session) Component.getInstance("hibernateSession");
+        String token = getToken(req.getRequestURL().toString());
+        if (StringUtil.isEmpty(token))
+            sendError(resp);
+        Criteria criteria = sess.createCriteria(VideoToken.class, "vt");
+        criteria.createCriteria("vt.chapter", "chap", Criteria.LEFT_JOIN);
+        criteria.add(Restrictions.eq("vt.memberId", uid));
+        criteria.add(Restrictions.eq("vt.token", token));
+        criteria.setMaxResults(1);
+        VideoToken vt = (VideoToken) criteria.uniqueResult();
+        if (vt == null)
+        {
+            sendError(resp);
+        }
+        Chapter chapter = vt.getChapter();
+        long expire = chapter.getDuration().getTime() + new Date().getTime();
+        //
+        String filePath = chapter.getUrl();
+        String res = req.getParameter("resolution");
+        filePath = getFileName(res, filePath);
+        File file = new File(filePath);
+        if (file.exists() == false)
+            sendError(resp);
+        MultipartFileSender.fromFile(file).with(req).with(resp).with(expire).serveResource();
+        // PersistenceContexts.instance().changeFlushMode(FlushModeType.MANUAL);
+        // if (r >= file.length())
+        // {
+        // sess.createQuery("delete from VideoToken vt where vt.id = :vtId").setParameter("vtId",
+        // vt.getId()).executeUpdate();
+        // sess.flush();
+        // }
+
+    }
 
     private static String getFileName(String res, String origFilePath)
     {
@@ -133,6 +161,64 @@ public class VideoPlayerServlet extends HttpServlet
     {
         String name = location.substring(location.lastIndexOf("/") + 1);
         return name.substring(0, name.indexOf("."));
+    }
+
+    private static void sendError(HttpServletResponse resp) throws IOException
+    {
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        resp.sendRedirect("error.seam");
+    }
+
+    private int getCounterValue()
+    {
+        AtomicInteger i = count.get();
+        if (i == null || i.intValue() < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return i.intValue();
+        }
+    }
+
+    /*
+     * Increments ThreadLocal counter value
+     */
+    private void incrementCounterValue()
+    {
+        AtomicInteger i = count.get();
+        if (i == null || i.intValue() < 0)
+        {
+            i = new AtomicInteger(0);
+            count.set(i);
+        }
+        i.incrementAndGet();
+    }
+
+    /*
+     * Decrements ThreadLocal counter value
+     */
+    private void decrementCounterValue()
+    {
+        AtomicInteger i = count.get();
+        if (i == null)
+        {
+            // we should never get here...
+            throw new IllegalStateException("Counter for nested ContextualHttpServletRequest was removed before it should be!");
+        }
+        if (i.intValue() > 0)
+        {
+            i.decrementAndGet();
+        }
+    }
+
+    /*
+     * Removes ThreadLocal counter
+     */
+    private void removeCounter()
+    {
+        count.remove();
     }
 
 }
