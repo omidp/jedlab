3c5b53662cf3816dcfd282866c078f4bea7433c4
omidp <omidpourhadi@gmail.com>
2016-09-16 16:20:04 +0430

    delete token after video ended

---
+++ jedlab-web/src/main/java/com/jedlab/DeleteTokenServlet.java
@@ -0,0 +1,42 @@
+package com.jedlab;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jedlab.framework.StringUtil;
+import com.jedlab.framework.TransactionalContextualHttpServletRequest;
+
+public class DeleteTokenServlet extends HttpServlet
+{
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
+    {
+
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
+    {
+
+        new TransactionalContextualHttpServletRequest(req) {
+
+            @Override
+            protected void workInTransaction()
+            {
+                String token = req.getParameter("tk");
+                if (StringUtil.isEmpty(token))
+                    return;
+                token = token.trim();
+                getEntityManager().createQuery("delete from VideoToken vt where vt.token = :token").setParameter("token", token)
+                        .executeUpdate();
+                getEntityManager().flush();
+            }
+        }.run();
+    }
+
+}
