982610c504643ec53f6ee7d5e62e262ff478934c
omidp <omidpourhadi@gmail.com>
2016-10-09 23:12:43 +0330

    date time minutes bug fix more than 60 mins support

--- jedlab-ejb/src/main/java/com/jedlab/framework/DateUtil.java
+++ jedlab-ejb/src/main/java/com/jedlab/framework/DateUtil.java
@@ -1,34 +1,50 @@
 package com.jedlab.framework;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.concurrent.TimeUnit;
 
 import com.omidbiz.persianutils.JalaliCalendar;
 
 public class DateUtil
 {
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
     public static String getDuration(Date duration)
     {
         Calendar cal = Calendar.getInstance();
         cal.setTime(duration);
-        cal.set(Calendar.YEAR, 1990);
-        cal.set(Calendar.MONTH, 1);
-        cal.set(Calendar.DATE, 1);
-        return sdf.format(cal.getTime());
+        //
+        Calendar endCal = getCalendarInstance();
+        endCal.set(Calendar.MINUTE, 0);
+        endCal.set(Calendar.SECOND, 0);
+        long millis = cal.getTimeInMillis() - endCal.getTimeInMillis();
+        return String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millis), TimeUnit.MILLISECONDS.toSeconds(millis)
+                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
     }
 
     public static Date getDuration(String hourMin) throws ParseException
     {
+        StringTokenizer tokenizer = new StringTokenizer(hourMin, ":");
+        if (tokenizer.countTokens() < 2)
+            throw new IllegalArgumentException("format must be mm:ss");
+        String min = tokenizer.nextToken();
+        String sec = tokenizer.nextToken();
+        Calendar cal = getCalendarInstance();
+        cal.set(Calendar.MINUTE, Integer.parseInt(min));
+        cal.set(Calendar.SECOND, Integer.parseInt(sec));
+        return cal.getTime();
+    }
+
+    private static Calendar getCalendarInstance()
+    {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(sdf.parse(hourMin));
         cal.set(Calendar.YEAR, 1990);
         cal.set(Calendar.MONTH, 1);
         cal.set(Calendar.DATE, 1);
-        return cal.getTime();
+        cal.set(Calendar.HOUR, 0);
+        return cal;
     }
 
     public static Date addDate(Date date, int field, int amount)
@@ -38,6 +54,5 @@ public class DateUtil
         cal.add(field, amount);
         return cal.getTime();
     }
-
 
 }
